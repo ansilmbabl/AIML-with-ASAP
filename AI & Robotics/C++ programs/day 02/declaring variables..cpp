c17bad413a8b9b43117808138a559b71c619031b
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-21 19:01:17 +0530

    Create declaring variables..cpp

---
+++ AI & Robotics/C++ programs/day 02/declaring variables..cpp
@@ -0,0 +1,23 @@
+//declaring variable in C++
+
+#include <iostream>
+using namespace std;
+
+int main()
+  
+{
+int num = 50;
+cout << num << "\n";
+char myalpha = 'c';
+cout << myalpha << "\n";
+double dec = 45.5555555555;
+cout << dec << "\n";
+return 0;
+
+//or
+cout << num << "\n" << myalpha << "\n" << dec;
+return 0;
+}
+
+
+
