f3f3eed721e18b6842b45b5b367f1edfd5c0229e
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-21 19:02:54 +0530

    Create summation.cpp

---
+++ AI & Robotics/C++ programs/day 02/summation.cpp
@@ -0,0 +1,18 @@
+//summation
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+int x = 5;
+int y = 9;
+int sum = x+y;
+cout <<"sum is "<<sum;
+return 0;
+
+//or
+int x=5,y=9;
+cout << "sum is" << x+y ;
+return 0;
+}
