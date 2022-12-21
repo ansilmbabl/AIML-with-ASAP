481634ba70acc477f9730d5f850932319dc6dca7
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-21 19:02:22 +0530

    Create line editings.cpp

---
+++ AI & Robotics/C++ programs/day 02/line editings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+//this is for comment
+cout <<"hello\n"; //print new line
+cout <<"my name is ......\t"; //print a tab
+cout <<"just typing........\"";//print "
+return 0;
+}
+
+
+/* long comments
+*/
+
+//short comments
