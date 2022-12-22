a6034d30efeae8ad6c9b7f03e04e10f8bd7f2133
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-22 19:11:32 +0530

    Create power of numbers.cpp

---
+++ AI & Robotics/C++ programs/day 03/power of numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main(){
+int n,m,res;
+
+cout<< "number 1: ";
+cin >> n;
+
+cout<< "number 2(power): ";
+cin >> m;
+
+res = pow(n,m);
+cout<< res;
+return 0;
+}
