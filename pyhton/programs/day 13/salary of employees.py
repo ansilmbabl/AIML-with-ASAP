936aaa54a0f700b1a1d08f47b701e03469d27fb0
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-22 13:14:57 +0530

    Update salary of employees.py

--- pyhton/programs/day 13/salary of employees.py
+++ pyhton/programs/day 13/salary of employees.py
@@ -39,3 +39,39 @@ print("name\tsalarypm\tabsent\tsalary")
 
 for i in l:
     i.details()
+    
+## or......................................
+#WAP using classes and object to take the employee name ,salary , absent days as input and calculate the total salary after dedcting the LOP of the employee and disaply it . asume month has 30 days 
+
+
+class salary:
+    count=0
+    def __init__(self):
+        salary.count+=1
+        print("employee ",salary.count)
+        self.n = input("name : ")
+        self.s = int(input("salary per month : "))
+        self.ab= int(input("number of days absent : "))
+        
+        
+    def sal(self):
+        return (self.s - self.ab * int((self.s/30)) )
+
+    def details(self):
+        print(f"{self.n}\t{self.s}\t{self.ab}\t{self.sal()}")
+
+
+nu = int(input("number of employees: "))
+l=[]
+
+
+for i in range(nu):
+    e = salary()
+    l.append(e)
+    l[i].sal()
+
+print("employee detail: ")
+print("name\tsalarypm\tabsent\tsalary")
+
+for i in l:
+    i.details()
