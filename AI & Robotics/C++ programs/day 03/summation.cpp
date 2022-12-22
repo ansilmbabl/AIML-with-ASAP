051dd8d7fb0a25852be5bb640036c428c563f166
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-22 19:12:09 +0530

    Create summation.cpp

---
+++ AI & Robotics/C++ programs/day 03/summation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int num1,num2,sum;
+
+cout<<"enter number1: ";
+cin>>num1;
+
+cout<<"enter number2: ";
+cin>>num2;
+
+sum = num1 + num2;
+cout<<"sum is: "<<sum;
+return 0;
+
+}
