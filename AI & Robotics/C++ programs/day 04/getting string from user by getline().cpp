707b2ccd41a39ac5b474d9adb5d43591e93671e7
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-23 18:58:31 +0530

    Create getting string from user by getline().cpp

---
+++ AI & Robotics/C++ programs/day 04/getting string from user by getline().cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+string x;
+cout << "welcome dear friend, type something: ";
+getline(cin,x);
+cout <<"your text is: "<<x;
+return 0;
+}
+
