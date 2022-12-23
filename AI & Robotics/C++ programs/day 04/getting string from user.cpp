13e931c9266c3d9ca490905f1b030e9a2d66f210
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-23 18:58:58 +0530

    Create getting string from user.cpp

---
+++ AI & Robotics/C++ programs/day 04/getting string from user.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+string x;
+cout << "welcome dear friend, type something: ";
+cin >>x;
+cout <<"your text is: "<<x;
+return 0;
+}
+
