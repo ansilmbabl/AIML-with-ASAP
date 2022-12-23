4754d0daf5a861cd1be8e9ad6b877feefb8aa248
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-23 18:58:00 +0530

    Create arithmetic operators.cpp

---
+++ AI & Robotics/C++ programs/day 04/arithmetic operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int x,y;
+
+cout<<"enter number 1: ";
+cin>>x;
+cout<<"enter number 2: ";
+cin>>y;
+
+cout<<"additon: "<< x+y <<"\nsubtraction: "<< x-y <<"\nmultiplication: "<< x*y <<"\ndivition: "<< x/y <<"\nmodulus: "<< x%y <<"\n";
+cout<<"increment number 1: "<< ++x <<"\n";
+cout<<"increment number 2: "<< ++y <<"\n";
+cout<<"decrement number 1: "<< --x <<"\n";
+cout<<"decrement number 2: "<< --y;
+
+}
