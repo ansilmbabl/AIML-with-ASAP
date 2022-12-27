a5537c5b2367a79a1d21e4331f2ea6f2a66c4a93
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-27 19:19:07 +0530

    Create days in a week.cpp

---
+++ AI & Robotics/C++ programs/day 05/days in a week.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int day ;
+cout<<"enter a number(1 to 7): ";
+cin>>day;
+
+switch(day){
+    case 1:
+        cout<<"mon";
+        break;
+    case 2:
+        cout<<"tue";
+        break;
+    case 3:
+        cout<<"wed";
+        break;
+    case 4:
+        cout<<"thu";
+        break;
+    case 5:
+        cout<<"fri";
+        break;
+    case 6:
+        cout<<"sat";
+        break;
+    case 7:
+        cout<<"sun";
+        break;
+        }
+}
