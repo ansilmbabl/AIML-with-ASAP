d4a7026c4c5497f8fe7c562c8afcda42a15e513a
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-30 19:27:37 +0530

    Create functions.cpp

---
+++ AI & Robotics/C++ programs/day 08/functions.cpp
@@ -0,0 +1,17 @@
+//functions
+
+#include <iostream>
+using namespace std;
+
+void number()
+{
+    int x;
+    cout<<"enter a number: ";
+    cin>>x;
+    cout<<"your number is " << x;
+}
+int main()
+{
+    number();
+    return 0;
+}
