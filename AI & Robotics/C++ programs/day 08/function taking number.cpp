59330563ddd7276f48f2e445dc2ba5a9df427665
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2022-12-30 19:26:41 +0530

    Create function taking number.cpp

---
+++ AI & Robotics/C++ programs/day 08/function taking number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void number()
+{
+    int x;
+    cout<<"enter a number: ";
+    cin>>x;
+    if (x == (int)
+    {
+       cout<<"your number is " << x;
+    }
+    else
+    {
+        cout<<"error...!";
+    }
+
+}
+int main()
+{
+    number();
+    return 0;
+}
