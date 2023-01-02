fa36e2d67d20515f885504cae8ddcb0c7658a7fb
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-01-02 19:19:06 +0530

    Create function with parameters (function definition after main() ).cpp

---
+++ AI & Robotics/C++ programs/day 09/function with parameters (function definition after main() ).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void name (string, int);
+
+int main()
+{
+    name("ansil",20);
+    name("XYZ",21);
+    name("ABCD",22);
+    return 0;
+}
+
+void name (string name, int age)
+{
+    cout<<"hiii "<< name<<", you are "<<age<<" years old"<<endl;
+}
