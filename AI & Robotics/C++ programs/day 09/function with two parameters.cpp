f57b4c771d08108f532546754d7419a05b4df4a7
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-01-02 19:20:03 +0530

    Create function with two parameters.cpp

---
+++ AI & Robotics/C++ programs/day 09/function with two parameters.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void name (string name, int age)
+{
+    cout<<"hiii "<< name<<", you are "<<age<<" years old"<<endl;
+}
+
+int main()
+{
+    name("ansil",20);
+    name("XYZ",21);
+    name("ABCD",22);
+    return 0;
+}
+
