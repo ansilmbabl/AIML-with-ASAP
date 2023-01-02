9c12374b435f0ad425b06a0b3657a1be8f015cfe
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-01-02 19:20:30 +0530

    Create maximum of two numbers.cpp

---
+++ AI & Robotics/C++ programs/day 09/maximum of two numbers.cpp
@@ -0,0 +1,31 @@
+//Write a program that ask for two numbers, compare them and show the maximun.Declare a function called max_twothat compares the numbers and returns the maximun.
+
+#include <iostream>
+using namespace std;
+
+int max_two(int x,int y)
+{
+    if (x==y)
+    {
+        cout << "same numbers";
+    }
+    else if (x>y)
+    {
+        return x;
+    }
+    else
+    {
+        return y;
+    }
+}
+
+int main()
+{
+    int x,y;
+    cout<<"first number: ";
+    cin>>x;
+    cout<<"second number: ";
+    cin>>y;
+    cout<<"max of these numbers : "<< max_two(x,y);
+    return 0;
+}
