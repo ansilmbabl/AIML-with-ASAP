8abf84c5e743f6bf4ed50e528a2164d883adefbe
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-01-03 19:31:02 +0530

    Create multiple objects.cpp

---
+++ AI & Robotics/C++ programs/day 10/multiple objects.cpp
@@ -0,0 +1,24 @@
+//objects and classes (OOPs)
+#include <iostream>
+using namespace std;
+
+class det{
+public:
+string name;
+int age;
+};
+
+int main()
+{
+det st1;
+    st1.name="ansil";
+    st1.age=21;
+    cout<<st1.name<<" is "<<st1.age<<" years old "<<endl;
+
+det st2;
+    st2.name="abcd";
+    st2.age=12;
+    cout<<st2.name<<" is "<<st2.age<<" years old "<<endl;
+
+return 0;
+}
