db858a04eb3495c8659ae46bb565bdbbb8a4a93e
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-01-03 19:30:40 +0530

    Create function definition outside class.cpp

---
+++ AI & Robotics/C++ programs/day 10/function definition outside class.cpp
@@ -0,0 +1,17 @@
+//objects and classes (OOPs)
+#include <iostream>
+using namespace std;
+
+class det{
+public:
+    void name();
+};
+
+void det::name(){
+    cout<<"ansil";}
+
+int main(){
+det st;
+    st.name();
+    return 0;
+}
