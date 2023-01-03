0755266a918e38ce0bbbbafdc243d57c79f01dff
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-01-03 19:31:23 +0530

    Create objects and classes (OOPs).cpp

---
+++ AI & Robotics/C++ programs/day 10/objects and classes (OOPs).cpp
@@ -0,0 +1,21 @@
+//objects and classes (OOPs)
+#include <iostream>
+using namespace std;
+
+class det{
+public:
+string name;
+int age;
+};
+
+int main()
+{
+det st;
+st.name="ansil";
+st.age=21;
+
+cout<<st.name<<endl;
+cout<<st.age;
+
+return 0;
+}
