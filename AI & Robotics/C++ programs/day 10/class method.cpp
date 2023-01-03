7eda0762cc0b047447a7c11324e9fd9972e2d43f
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-01-03 19:30:06 +0530

    Create class method.cpp

---
+++ AI & Robotics/C++ programs/day 10/class method.cpp
@@ -0,0 +1,16 @@
+//objects and classes (OOPs)
+#include <iostream>
+using namespace std;
+
+class det{
+public:
+    void name(string x){
+    cout<<"hi "<<x;
+    }
+};
+
+int main(){
+det st;
+    st.name("ansil");
+    return 0;
+}
