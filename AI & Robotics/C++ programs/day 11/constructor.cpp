9b8fb50ea2e6a1140ae0e829764b2cd2a0e06c85
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-01-04 19:04:04 +0530

    Create constructor.cpp

---
+++ AI & Robotics/C++ programs/day 11/constructor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class det
+{
+public:
+    det() //constructor
+    {
+        cout<<"welcome here.........";
+    }
+};
+
+int main()
+{
+    det stud;
+    return 0;
+}
