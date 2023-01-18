0cbe5a1347a91f368c74ae85db81bef868f35349
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-01-18 20:29:01 +0530

    Update dictionary keys and values using lambda function.py

--- pyhton/programs/day 29/dictionary keys and values using lambda function.py
+++ pyhton/programs/day 29/dictionary keys and values using lambda function.py
@@ -12,3 +12,15 @@ key = list(map(lambda x:x.keys(),company))
 print(key)
 value = list(map(lambda x:x.values(),company))
 print(value)
+
+## for getting from a specific department (filtration)
+finance_det = list(filter(lambda x:x["department"]=="finance",company))
+print(finance_det)
+
+## print salary of person working in finance
+s = list(filter(lambda x:x["department"]=="finance",company))
+print(s)
+
+sa = list(map(lambda x:x["salary"] if x["department"]=="finance" else "nil",company))
+print(sa)
+
