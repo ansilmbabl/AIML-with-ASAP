5775afe7832cb2042857189913fa8663bf532177
ansilmbabl <86063895+ansilmbabl@users.noreply.github.com>
2023-02-09 21:33:35 +0530

    Create histogram of black image.py

---
+++ opnCV/day 23/histogram of black image.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = np.zeros((200,200),np.uint8)
+cv2.imshow("img",img)
+
+plt.hist(img.ravel(),256,(0,256))
+plt.show()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
